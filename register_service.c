6d4cddae288fe5e5adc7b0a085e26fab98b444a9
Himanshu Neema <gigapotentials@gmail.com>
2013-09-28 02:07:55 +0530

    to make service discoverable by android

--- register_service.c
+++ register_service.c
@@ -3,10 +3,11 @@
 #include <bluetooth/sdp_lib.h>
 #include <stdlib.h>
 
-sdp_session_t *register_service(int chnel)
+sdp_session_t *register_service(int chnel, int l2_chnel)
 {
     uint32_t service_uuid_int[] = { 0, 0, 0, 0xABCD };
     uint8_t rfcomm_channel = chnel;
+    uint16_t l2cap_channel = l2_chnel;
     const char *service_name = "Roto-Rooter Data Router";
     const char *service_dsc = "An experimental plumbing router";
     const char *service_prov = "Roto-Rooter";
@@ -16,14 +17,17 @@ sdp_session_t *register_service(int chnel)
                *rfcomm_list = 0,
                *root_list = 0,
                *proto_list = 0, 
-               *access_proto_list = 0;
-    sdp_data_t *channel = 0, *psm = 0;
+               *access_proto_list = 0,
+               *class_id_list = 0;
+    sdp_data_t *channel = 0, *psm = 0, *l2cap_port = 0;
 
     sdp_record_t *record = sdp_record_alloc();
 
     // set the general service ID
     sdp_uuid128_create( &svc_uuid, &service_uuid_int );
-    sdp_set_service_id( record, svc_uuid );
+    class_id_list = sdp_list_append(0, &svc_uuid);
+    //sdp_set_service_id( record, svc_uuid );
+    sdp_set_service_classes( record, class_id_list);
 
     // make the service record publicly browsable
     sdp_uuid16_create(&root_uuid, PUBLIC_BROWSE_GROUP);
@@ -33,7 +37,9 @@ sdp_session_t *register_service(int chnel)
     // set l2cap information
     sdp_uuid16_create(&l2cap_uuid, L2CAP_UUID);
     l2cap_list = sdp_list_append( 0, &l2cap_uuid );
-    proto_list = sdp_list_append( 0, l2cap_list );
+    l2cap_port = sdp_data_alloc(SDP_UINT16, &l2cap_channel);
+		sdp_list_append( l2cap_list, l2cap_port);
+		proto_list = sdp_list_append( 0, l2cap_list );
 
     // set rfcomm information
     sdp_uuid16_create(&rfcomm_uuid, RFCOMM_UUID);
@@ -68,12 +74,12 @@ sdp_session_t *register_service(int chnel)
 
 int main(int argc, char *argv[])
 {
-    if( argc < 2)
+    if( argc < 3)
     {
-      printf("usage: %s <rfcomm-channel>\n", argv[0]);
+      printf("usage: %s <rfcomm-channel> <l2cap-channel>\n", argv[0]);
       exit(1);
     }
-   	sdp_session_t *session = register_service(atoi(argv[1]));
+   	sdp_session_t *session = register_service(atoi(argv[1]), atoi( argv[2]));
     printf("service registered, sleeping now zzzzzz (verify using service-search-sdp.c)\n");
 		sleep(1000); 
     return 0;
